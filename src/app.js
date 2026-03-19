02bcffa50912dbf2a07b3debbc76293f1e0de039
joao-meyer <joaoparizotimeyer@gmail.com>
2026-03-19 17:06:23 -0300

    adding route files.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const express = require("express")
 
-const userController = require("./controllers/userController")
-
 const app = express()
 
 app.get("/", (request, response) => {
@@ -9,15 +7,5 @@ app.get("/", (request, response) => {
 })
 
 app.use(express.json())
-
-app.post("/users", userController.createUser)
-
-app.get("/users", userController.getAllUsers)
-
-app.get("/users/:id", userController.getUserById)
-
-app.put("/users/:id", userController.updateUser)
-
-app.delete("/users/:id", userController.deleteUser)
 
 module.exports = app
